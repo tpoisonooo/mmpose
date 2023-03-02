63af7ae38fb23c391b4faac510b0ab1b3a0e11e6
lupeng <penglu2097@gmail.com>
2023-03-02 13:31:04 +0800

    update metafiles & add model list

---
+++ .dev_scripts/benchmark_options.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+
+third_part_libs = [
+    'pip install -r ../requirements/albu.txt',
+    'pip install -r ../requirements/runtime.txt',
+    'pip install -r ../requirements/poseval.txt',
+]
+
+default_floating_range = 0.005
